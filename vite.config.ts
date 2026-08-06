939ab0b66bc1a1bc64ce5fe349949917d1451434
Kunal <131905488+ROYALKINGKUNAL9090@users.noreply.github.com>
2026-08-07 02:03:20 +0530

    Initial commit//1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-interface ImportMetaEnv {
-  readonly VITE_GEMINI_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+export default defineConfig({
+  plugins: [react()],
+});
